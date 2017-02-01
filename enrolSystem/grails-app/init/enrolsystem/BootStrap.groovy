70c02c09d8316c971dc9d33a0107b45f7b5fe0be
b5028967 <b5028967@my.shu.ac.uk>
2017-02-01 11:45:31 +0000

    created domain classes for stuent, lecutre and module

---
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -0,0 +1,22 @@
+package com.enrol
+
+class BootStrap {
+
+    def init = { servletContext -> 
+		def computing = new Course(
+		title:'BSc Hon Computing',
+		code:'COMP234',
+		leader:'Dr Mary Poppins',
+		department:'Computing',
+		startDate:new Date('11/11/2016'),
+		endDate:new Date ('11/11/2020'),
+		numberOfStudents:45,
+		description:'''Add some text here''',
+		studyMode:'Full-time',
+		tuitionFees:9000
+		).save()
+		
+    }
+    def destroy = {
+    }
+}
